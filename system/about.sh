4ddf39cc6060d2eed2a5d59a8202ae484aaedbdf
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-08-12 00:53:41 +0800

    Add files via upload

---
+++ system/about.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#wget https://github.com/${GitUser}/
+GitUser="NevermoreSSH"
+#IZIN SCRIPT
+MYIP=$(curl -sS ipv4.icanhazip.com)
+clear
+yl='\e[031;1m'
+bl='\e[36;1m'
+gl='\e[32;1m'
+clear
+yl='\e[031;1m'
+bl='\e[36;1m'
+gl='\e[32;1m'
+clear
+echo ""
+echo -e "SCRIPT" | lolcat
+figlet " By Nevermore" | lolcat
+echo -e "\e[32;1m.-----------------------------------------------.\e[0m"
+echo -e "\e[32;1m|               \e[36;1mINFO SCRIPT VPS\e[0m                 \e[32;1m|\e[0m"
+echo -e "\e[32;1m'-----------------------------------------------'\e[0m"
+echo -e "           \e[031;1m> Script By Nevermore <\e[0m" | lolcat
+echo -e " \e[0;32m_______________________________________________\e[0m"
+echo -e "\e[0;32m|    \e[1;35mFor OS Debian 10 & Ubuntu 20.04 64 bit     \e[0;32m|\e[0m"
+echo -e "\e[0;32m|  \e[1;35mFor VPS with KVM and VMWare virtualization   \e[0;32m|\e[0m"
+echo -e "\e[0;32m|               \e[1;35mI HOPE YOU HAPPY                \e[0;32m|\e[0m"
+echo -e "\e[0;32m|_______________________________________________\e[0;32m|\e[0m"
+echo -e "\e[0;32m|                   \e[1;36mTHANKS TO                   \e[0;32m|\e[0m"
+echo -e "\e[0;32m|_______________________________________________\e[0;32m|\e[0m"
+echo -e "\e[0;32m|                \e[1;35mkey                    \e[0;32m|\e[0m"
+echo -e "\e[0;32m|                   \e[1;35mGOD                   \e[0;32m|\e[0m"
+echo -e "\e[0;32m|                   \e[1;35mMy Family & Friends      \e[0;32m|\e[0m"
+echo -e "\e[0;32m|                    \e[1;35mGoogle                     \e[0;32m|\e[0m"
+echo -e "\e[0;32m|_______________________________________________|\e[0m"
+echo -e "\e[0;32m|_______________\e[36;1mTHANK YOU FOR SUPPORT\e[0m\e[0;32m________________|\e[0m"
+echo -e "               \e[0;32m'----------------'\e[0m"
