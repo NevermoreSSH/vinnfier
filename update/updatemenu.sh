11553529cec46f4fe4619945442a4a8881fd0f3f
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-08-12 12:46:43 +0800

    Update updatemenu.sh

--- update/updatemenu.sh
+++ update/updatemenu.sh
@@ -40,7 +40,7 @@ cd /usr/bin
 wget -O backup "${REPO}backup.sh"
 wget -O dns "${REPO}dns.sh"
 wget -O netf "${REPO}netf.sh"
-wget -O sustem "${REPO}system.sh"
+wget -O system "${REPO}system.sh"
 wget -O menu "${REPO}menu.sh"
 
 chmod +x backup
