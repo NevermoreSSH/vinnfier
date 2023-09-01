f9a68670361c541c24c035aac43d01a7e80041b2
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-09-01 22:15:51 +0800

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# ==================================================
+# =================================================
 
 # initializing var
 export DEBIAN_FRONTEND=noninteractive
