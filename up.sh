0d28e4566b8f2391dfd30d752db101bc5f57d653
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-12-09 11:58:05 +0800

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-###########- COLOR CODE -##############
+###########- COLOR CODE -###############
 echo -e " [INFO] Downloading Update File"
 sleep 2
 # hapus menu
