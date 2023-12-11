b72479a55ac60c6b7f0c936daad52173aaec1dce
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-12-11 12:51:12 +0800

    Update themes.sh

--- menu/themes.sh
+++ menu/themes.sh
@@ -4,6 +4,9 @@
 GitUser="NevermoreSSH"
 #IZIN SCRIPT
 MYIP=$(curl -sS ipv4.icanhazip.com)
+MYIP=$(curl -s ipinfo.io/ip )
+MYIP=$(curl -sS ipv4.icanhazip.com)
+MYIP=$(curl -sS ifconfig.me )
 # PROVIDED
 creditt=$(cat /root/provided)
 # BANNER COLOUR
