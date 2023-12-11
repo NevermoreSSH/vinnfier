059064f04c532b6bece4dcb02ff970ac8b280fb6
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-12-11 13:23:09 +0800

    Update cfp.sh

--- cloud/cfp.sh
+++ cloud/cfp.sh
@@ -3,6 +3,9 @@
 GitUser="NevermoreSSH"
 #IZIN SCRIPT
 MYIP=$(curl -sS ipv4.icanhazip.com)
+MYIP=$(curl -s ipinfo.io/ip )
+MYIP=$(curl -sS ipv4.icanhazip.com)
+MYIP=$(curl -sS ifconfig.me )
 clear
 red='\e[1;31m'
 green='\e[0;32m'
