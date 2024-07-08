ca37ba71ba759953f5fba9564f271701245a73b1
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2024-07-08 19:18:12 +0800

    Update menu.sh

--- update/menu.sh
+++ update/menu.sh
@@ -22,39 +22,18 @@ cpu_usage1="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum}')"
 cpu_usage="$((${cpu_usage1/\.*/} / ${corediilik:-1}))"
 cpu_usage+=" %"
 #Download/Upload today
-#dtoday="$(vnstat -i eth0 | grep "today" | awk '{print $2" "substr ($3, 1, 1)}')"
-#utoday="$(vnstat -i eth0 | grep "today" | awk '{print $5" "substr ($6, 1, 1)}')"
-#ttoday="$(vnstat -i eth0 | grep "today" | awk '{print $8" "substr ($9, 1, 1)}')"
+dtoday="$(vnstat -i eth0 | grep "today" | awk '{print $2" "substr ($3, 1, 1)}')"
+utoday="$(vnstat -i eth0 | grep "today" | awk '{print $5" "substr ($6, 1, 1)}')"
+ttoday="$(vnstat -i eth0 | grep "today" | awk '{print $8" "substr ($9, 1, 1)}')"
 #Download/Upload yesterday
-#dyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $2" "substr ($3, 1, 1)}')"
-#uyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $5" "substr ($6, 1, 1)}')"
-#tyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $8" "substr ($9, 1, 1)}')"
+dyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $2" "substr ($3, 1, 1)}')"
+uyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $5" "substr ($6, 1, 1)}')"
+tyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $8" "substr ($9, 1, 1)}')"
 #Download/Upload current month
-#dmon="$(vnstat -i eth0 -m | grep "$(date +"%b '%y")" | awk '{print $3" "substr ($4, 1, 1)}')"
-#umon="$(vnstat -i eth0 -m | grep "$(date +"%b '%y")" | awk '{print $6" "substr ($7, 1, 1)}')"
-#tmon="$(vnstat -i eth0 -m | grep "$(date +"%b '%y")" | awk '{print $9" "substr ($10, 1, 1)}')"
-# yesterday
-dyest="$(vnstat | grep yesterday | awk '{print $2" "substr ($3, 1, 1)}')"
-uyest="$(vnstat | grep yesterday | awk '{print $5" "substr ($6, 1, 1)}')"
-tyest="$(vnstat | grep yesterday | awk '{print $8" "substr ($9, 1, 1)}')"
-# today
-dtoday="$(vnstat | grep today | awk '{print $2" "substr ($3, 1, 1)}')"
-utoday="$(vnstat | grep today | awk '{print $5" "substr ($6, 1, 1)}')"
-ttoday="$(vnstat | grep today | awk '{print $8" "substr ($9, 1, 1)}')"
-# montly
-dmon="$(vnstat -m | grep "$(date '+%Y-%m')" | awk '{print $2" "substr ($3, 1, 1)}')"
-umon="$(vnstat -m | grep "$(date '+%Y-%m')" | awk '{print $5" "substr ($6, 1, 1)}')"
-tmon="$(vnstat -m | grep "$(date '+%Y-%m')" | awk '{print $8" "substr ($9, 1, 1)}')"
+dmon="$(vnstat -i eth0 -m | grep "$(date +"%b '%y")" | awk '{print $3" "substr ($4, 1, 1)}')"
+umon="$(vnstat -i eth0 -m | grep "$(date +"%b '%y")" | awk '{print $6" "substr ($7, 1, 1)}')"
+tmon="$(vnstat -i eth0 -m | grep "$(date +"%b '%y")" | awk '{print $9" "substr ($10, 1, 1)}')"
 # Fixed vnstat for ubuntu v2
-#dmon="$(vnstat -m | grep $(date +%Y-%m) | awk '{print $2, $3}')"
-#umon="$(vnstat -m | grep $(date +%Y-%m) | awk '{print $5, $6}')"
-#tmon="$(vnstat -m | grep $(date +%Y-%m) | awk '{print $8, $9}')"                     # total usage current monthly
-#dtoday="$(vnstat -d | grep $(date +%Y-%m-%d) | awk '{print $2, $3}')"
-#utoday="$(vnstat -d | grep $(date +%Y-%m-%d) | awk '{print $5, $6}')"
-#ttoday="$(vnstat -d | grep $(date +%Y-%m-%d) | awk '{print $8, $9}')"                # today usage
-#dyest="$(vnstat -d | grep $(date -d 'yesterday' +%Y-%m-%d) | awk '{print $2, $3}')"
-#uyest="$(vnstat -d | grep $(date -d 'yesterday' +%Y-%m-%d) | awk '{print $5, $6}')"
-#tyest="$(vnstat -d | grep $(date -d 'yesterday' +%Y-%m-%d) | awk '{print $8, $9}')"  # yesterday usage
 # TOTAL ACC CREATE VMESS WS
 vmess=$(grep -c -E "^### " "/usr/local/etc/xray/config.json")
 # TOTAL ACC CREATE  VLESS WS
