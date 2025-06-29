a4b068aebcc2a3841b8829be866ae382cfba0b90
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2025-06-29 22:14:45 +0800

    Update system.sh

--- update/system.sh
+++ update/system.sh
@@ -56,7 +56,7 @@ read -p "        Please Input Number  [1-99 or x] :  "  sys
 echo -e ""
 case $sys in
 1)
-sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill;add-host
+lsof -t -i tcp:80 -s tcp:listen | xargs kill;add-host
 ;;
 2)
 certv2ray
